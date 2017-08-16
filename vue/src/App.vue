b78bfa5eed35e8551c7f9c9d3260dd5f991e34ba
lanry0523 <sendou00002@163.com>
2017-08-16 15:54:07 +0800

    框架改变

---
+++ vue/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <hello></hello>
+  </div>
+</template>
+
+<script>
+import Hello from './components/Hello'
+
+export default {
+  name: 'app',
+  components: {
+    Hello
+  }
+}
+</script>
+
+<style>
+/*#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  background-image: url("/static/images/灵梦.jpg");
+  background-repeat:repeat;
+  background-size:100% 100%;
+  -moz-background-size:100% 100%;
+  height: -webkit-fill-available;
+}*/
+</style>
---
+++ vue/src/components/Hello.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="hello">
+    <form id="slick-login">
+      <label for="username">username</label><input type="text" name="username" placeholder="me@tutsplus.com">
+      <label for="password">password</label><input type="password" name="password" placeholder="password">
+      <input type="submit" value="Log In">
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'hello',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
